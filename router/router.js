f9a647738c221e1e43f34566d0ed82fb2fe6985b
Johnathan Pylant <johnathan.pylant@student.fullsail.edu>
2023-01-16 14:03:55 -0500

    Added router request listener and fulfilled design pattern S/L/R

---
+++ router/router.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+// http://localhost:3000/excercise
+router.get("/", (req,res, next)=>{
+    res.status(200).json({
+        message: 'Successful - GET',
+        metadata:{
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+
+// http://localhost:3000/excercise/34
+router.get("/:id", (req,res,next)=>{
+    const id = req.params.id;
+    res.status(200).json({
+        message:'Successful - GET by ID',
+        id: id,
+        metadata:{
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+module.exports = router;
